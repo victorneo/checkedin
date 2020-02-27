8f327c96105013562af4609ad318940ea1c3c182
Victor Neo <icyisamu@gmail.com>
2020-02-28 00:14:15 +0800

    Add tests for login and index

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects, assertTemplateUsed
+from django.test import Client
+import pytest
 
-# Create your tests here.
+
+def test_index(client, admin_client):
+    url = reverse('webapp.index')
+    resp = client.get(url)
+    assertRedirects(resp, reverse('webapp.login')+'?next='+url)
+
+    resp = admin_client.get(url)
+    assertTemplateUsed('index.html')
+
+
+def test_login(client, admin_client):
+    url = reverse('webapp.login')
+    resp = client.get(url)
+    assertTemplateUsed(resp, 'login.html')
+
+    resp = client.post(url, {'username': 'asd', 'password': 'asd'})
+    assertTemplateUsed(resp, 'login.html')
+    assert resp.context.get('failed') == True
+
+    resp = client.post(url, {'username': 'admin', 'password': 'password'})
+    assertRedirects(resp, reverse('webapp.index'))
