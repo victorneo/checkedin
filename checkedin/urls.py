17836e4f8ae6696e31c5e3f5916a42bacc90e055
Victor Neo <icyisamu@gmail.com>
2020-02-27 23:33:31 +0800

    Remove crypto for actpub

--- checkedin/urls.py
+++ checkedin/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include('actpub.urls')),
     path('', include('webapp.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
